de1dd102bea36edbde176f674bf771e5992971bd
marcos <marcosvila96@gmail.com>
2023-08-13 14:49:25 -0300

    update

--- server/db.js
+++ server/db.js
@@ -2,11 +2,7 @@ const {Pool} = require('pg')
 const {db} = require('./config.js')
 
 const pool = new Pool({
-  user: 'postgres',
-  password: 'Flatron2009',
-  host: 'localhost',
-  port: 5432,
-  database: 'jodifydb'
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
 console.log('conection success')
 
