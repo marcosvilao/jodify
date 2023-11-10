d437e606ab99913a48262d2451c2ea44401616e7
marcos <marcosvila96@gmail.com>
2023-11-10 15:35:28 -0300

    update date field

--- client/src/components/Calendar/DayPickStyles.js
+++ client/src/components/Calendar/DayPickStyles.js
@@ -3,20 +3,6 @@ import { styled } from '@mui/material/styles';
 import theme from '../../jodifyStyles';
 
 export const DayPick = styled(DatePicker)({
-  background: theme.jodify_colors._background_black,
-    color: theme.jodify_colors._text_white,
     display: 'flex',
-    width: '90%',
-    '& .MuiInput-underline:after': {
-        borderBottomColor: theme.jodify_colors._icons_primary,
-      },
-      '& .MuiInputLabel-root': {
-        color: theme.jodify_colors._text_white,
-      },
-      '& .css-1x51dt5-MuiInputBase-input-MuiInput-input': {
-        color: theme.jodify_colors._text_white,
-      },
-      '& .css-i4bv87-MuiSvgIcon-root': {
-        fill: theme.jodify_colors._text_white
-      }
+    width: '90%'
 })
