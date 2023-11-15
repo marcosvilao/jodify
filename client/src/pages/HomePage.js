94ee3875b97ce933d75fbc233af7cbca9225fe20
marcos <marcosvila96@gmail.com>
2023-11-15 12:23:23 -0300

    update filters

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from '../components/Jodify-Title/Title.jsx'
 import Grid from '../components/Events-grid/Grid.jsx'
 import { GlobalContainer } from '../jodifyStyles.js'
 import SearchEvents from '../components/Search-field/SearchEvents.jsx'
 import FilterEvents from '../components/Filters/FilterEvents.jsx'
 import { StickyHeader } from './HomePageStyles.js'
+import { fetchCities, fetchTypes } from '../storage/actions.js'
 
 function HomePage() {
+  const [cities, setCities] = useState([])
+  const [types, setTypes] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      setCities(await fetchCities());
+      setTypes(await fetchTypes());
+    }
+    fetchData();
+  }, []);
+
   return (
     <GlobalContainer>
     <Title/>
     <StickyHeader>
       <div style={{marginTop: '16px'}}>
     <SearchEvents/>
-    <FilterEvents/>
+    <FilterEvents cities={cities} types={types}/>
       </div>
     </StickyHeader>
     <Grid/>
