6a818b996b2ba1c88fa5eea689eee805bf44a39f
marcos <marcosvila96@gmail.com>
2023-11-16 19:46:53 -0300

    orden alfabetico

--- server/controllers/type.controller.js
+++ server/controllers/type.controller.js
@@ -1,19 +1,16 @@
 const pool = require('../db')
 
-
-
-
-
 const getTypes = async (req, res, next) => {
     try {
+        const allTypes = await pool.query('SELECT * FROM type ORDER BY "type_name" ASC')
 
-        const allTypes = await pool.query('SELECT * from type')
-        if(allTypes.rows) res.status(200).json(allTypes.rows)
-        else{
-            res.status(404).send({message: 'Cannot receive types from Database, please try again'})
-            }
+        if (allTypes.rows) {
+            res.status(200).json(allTypes.rows);
+        } else {
+            res.status(404).send({ message: 'Cannot receive types from Database, please try again' });
+        }
     } catch (error) {
-        next(error)
+        next(error);
     }
 }
 
