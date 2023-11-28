b62dc7b6f8b33b4bd36938da081a5dbcb78ec9a6
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2023-11-28 15:46:36 -0300

    texto filtro no encontrados se ve arriba para celulares, modificacion de margin top

--- client/src/components/Event-not-found/EventNotFound.jsx
+++ client/src/components/Event-not-found/EventNotFound.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
-import theme from '../../jodifyStyles';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import theme from "../../jodifyStyles";
+import { useSelector } from "react-redux";
 
 function EventNotFound() {
-  const isSearching = useSelector(state => state.search.isSearching)
+  const isSearching = useSelector((state) => state.search.isSearching);
+  const [MarginTop, setMarginTop] = useState(50);
+
+  useEffect(() => {
+
+    if (window.innerWidth < 500) {
+      setMarginTop(5);
+    }
+  }, []);
+
   return (
     <div
-    style={{
-        display: 'flex',
-        width: '100%',
-        marginTop: isSearching ? '10%' : '50%',
-        height: '14px',
-        justifyContent: 'center',
-        }}
+      className="div-event-not-found"
+      style={{
+        display: "flex",
+        width: "100%",
+        marginTop: isSearching ? "10%" : `${MarginTop}%`,
+        height: "14px",
+        justifyContent: "center",
+      }}
     >
-        <p
+      <p
         style={{
-            color: theme.jodify_colors._text_white,
-            fontFamily: theme.fonts._title_font_family,
-            fontSize: '16px',
-            fontStyle: 'normal',
-            fontWeight: '500',
-            lineHeight: 'normal',
+          color: theme.jodify_colors._text_white,
+          fontFamily: theme.fonts._title_font_family,
+          fontSize: "16px",
+          fontStyle: "normal",
+          fontWeight: "500",
+          lineHeight: "normal",
         }}
-        >No se encontraron eventos.</p>
+      >
+        No se encontraron eventos.
+      </p>
     </div>
-  )
+  );
 }
 
-export default EventNotFound
+export default EventNotFound;
