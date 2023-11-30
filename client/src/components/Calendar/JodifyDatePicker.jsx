ed18d2ce8c82099d2eb96b2c0a9cbf620e88240b
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2023-11-30 13:28:19 -0300

    arreglo date picker

--- client/src/components/Calendar/JodifyDatePicker.jsx
+++ client/src/components/Calendar/JodifyDatePicker.jsx
@@ -1,54 +1,70 @@
-import * as React from 'react';
-import dayjs from 'dayjs';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { Box } from '@mui/material';
-import theme from '../../jodifyStyles';
-import { DateRange } from './JodifyDatePickerStyles';
-import { useState} from 'react';
-import esLocale from 'date-fns/locale/es';
+import * as React from "react";
+import dayjs from "dayjs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { Box } from "@mui/material";
+import theme from "../../jodifyStyles";
+import { DateRange } from "./JodifyDatePickerStyles";
+import { useState } from "react";
+import esLocale from "date-fns/locale/es";
 
+export default function JodifyDatePicker({
+  setOpen,
+  setIsOpen,
+  setDateFilter,
+  setDefaultValues,
+}) {
+  const [dates, setDates] = useState(setDefaultValues);
+  const defaultDates =
+    setDefaultValues.length > 0
+      ? setDefaultValues.map((date) => dayjs(date))
+      : undefined;
 
-
-
-export default function JodifyDatePicker({setOpen, setIsOpen, setDateFilter, setDefaultValues}) {
-  const [dates, setDates] = useState(setDefaultValues)
-  const defaultDates = setDefaultValues.length > 0 ? setDefaultValues.map(date => dayjs(date)) : undefined
   const getDates = (value) => {
-      value[0].setHours(9,0,0,0)
-      if(!value[0] && !value[1]){
-        return
+    if (!value[0]) {
+      return;
+    } else {
+      value[0].setHours(9, 0, 0, 0);
+      if (!value[0] && !value[1]) {
+        return;
       }
-      if(!value[1]){
-        value[1] = value[0]
+      if (!value[1]) {
+        value[1] = value[0];
       }
       setDateFilter(value);
       setIsOpen(false);
+    }
   };
 
   return (
-
-            <Box
-            sx={{zIndex: '3',position: 'absolute', marginTop: '50vh', marginLeft: '5%'}}
-            >
-              <LocalizationProvider dateAdapter={AdapterDayjs} locale={esLocale}>
-              <DateRange
-              disablePast
-              localeText={{ start: 'Inicio', end: 'Fin' }}
-              locale={esLocale}
-              label="disabled"
-              toolbarTitle="Seleccionar rango de fechas"
-              disableHighlightToday={true}
-              onChange={(value) => setDates([value[0]?.$d, value[1]?.$d])}
-              onClose={() => setIsOpen(false)}
-              onAccept={() => getDates(dates)}
-              defaultValue={defaultDates}
-              sx={{ fontSize: '16px', color: theme.jodify_colors._text_white , bgcolor: theme.jodify_colors._background_gray, borderRadius: theme.jodify_borders._lg_border_radius }}
-            />
-            </LocalizationProvider>
-            </Box>
-
-
-      
+    <Box
+      sx={{
+        zIndex: "3",
+        position: "absolute",
+        marginTop: "50vh",
+        marginLeft: "5%",
+      }}
+    >
+      <LocalizationProvider dateAdapter={AdapterDayjs} locale={esLocale}>
+        <DateRange
+          disablePast
+          localeText={{ start: "Inicio", end: "Fin" }}
+          locale={esLocale}
+          label="disabled"
+          toolbarTitle="Seleccionar rango de fechas"
+          disableHighlightToday={true}
+          onChange={(value) => setDates([value[0]?.$d, value[1]?.$d])}
+          onClose={() => setIsOpen(false)}
+          onAccept={() => getDates(dates)}
+          defaultValue={defaultDates}
+          sx={{
+            fontSize: "16px",
+            color: theme.jodify_colors._text_white,
+            bgcolor: theme.jodify_colors._background_gray,
+            borderRadius: theme.jodify_borders._lg_border_radius,
+          }}
+        />
+      </LocalizationProvider>
+    </Box>
   );
 }
