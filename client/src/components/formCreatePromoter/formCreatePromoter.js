8ef7ed82b94f0cdfb17754941697621aae2e6fa6
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2023-12-13 14:20:07 -0300

    cambio numeros priority

--- client/src/components/formCreatePromoter/formCreatePromoter.js
+++ client/src/components/formCreatePromoter/formCreatePromoter.js
@@ -11,7 +11,7 @@ function FormCreatePromoter() {
     instagram: "",
     priority: "",
   });
-  const arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  const arrayNumbers = [1, 2, 3, 4];
 
   const onChangeInput = (e) => {
     setDataPost({
