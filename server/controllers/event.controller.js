f20c3724ea22e422b16233eec917d4ee1c76a4ce
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2023-12-18 14:48:51 -0300

    ruta mas controller para traer eventos del dia con sus productoras ordenado por prioridad

--- server/controllers/event.controller.js
+++ server/controllers/event.controller.js
@@ -1,152 +1,208 @@
-const pool = require('../db')
-const {linkScrap} = require('../Brain/getEventData')
-const { v4: uuidv4 } = require('uuid');
-const { types } = require('pg');
-
+const pool = require("../db");
+const { linkScrap } = require("../Brain/getEventData");
+const { v4: uuidv4 } = require("uuid");
+const { types } = require("pg");
+const { DateTime } = require("luxon");
 
 const getEvents = async (req, res, next) => {
-    try {
-        const currentDate = new Date();
-        currentDate.setDate(currentDate.getDate() - 1);
-        const options = { timeZone: 'America/Argentina/Buenos_Aires' };
-        const argentinaTime = currentDate.toLocaleString('en-US', options);
-        const query = ('SELECT * FROM event WHERE event_date >= $1');
-        const values = [argentinaTime];
-        const allEvents = await pool.query(query, values);
-        if (!allEvents.rows) {
-            res.status(404).send({ message: 'Cannot receive events from Database, please try again' });
-            return;
-        }
-        const groupedEventsArray = [];
-        const groupedEvents = {};
-        allEvents.rows.forEach(event => {
-            let eventDate = event.event_date;
-            if (!groupedEvents[eventDate]) {
-                groupedEvents[eventDate] = [];
-            }
-            groupedEvents[eventDate].push(event);
-        });
-
-        for (const date in groupedEvents) {
-            if (groupedEvents.hasOwnProperty(date)) {
-                const obj = {};
-                obj[date] = groupedEvents[date];
-                groupedEventsArray.push(obj);
-            }
-        }
-
-                groupedEventsArray.sort((a, b) => {
-                    const dateA = Object.keys(a)[0];
-                    const dateB = Object.keys(b)[0];
-                    
-                    const dateObjA = new Date(dateA);
-                    const dateObjB = new Date(dateB);
-        
-                    return dateObjA - dateObjB;
-                });
-
-
-        res.status(200).json(groupedEventsArray);
-    } catch (error) {
-        next(error);
+  try {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() - 1);
+    const options = { timeZone: "America/Argentina/Buenos_Aires" };
+    const argentinaTime = currentDate.toLocaleString("en-US", options);
+    const query = "SELECT * FROM event WHERE event_date >= $1";
+    const values = [argentinaTime];
+    const allEvents = await pool.query(query, values);
+    if (!allEvents.rows) {
+      res.status(404).send({
+        message: "Cannot receive events from Database, please try again",
+      });
+      return;
     }
+    const groupedEventsArray = [];
+    const groupedEvents = {};
+    allEvents.rows.forEach((event) => {
+      let eventDate = event.event_date;
+      if (!groupedEvents[eventDate]) {
+        groupedEvents[eventDate] = [];
+      }
+      groupedEvents[eventDate].push(event);
+    });
+
+    for (const date in groupedEvents) {
+      if (groupedEvents.hasOwnProperty(date)) {
+        const obj = {};
+        obj[date] = groupedEvents[date];
+        groupedEventsArray.push(obj);
+      }
+    }
+
+    groupedEventsArray.sort((a, b) => {
+      const dateA = Object.keys(a)[0];
+      const dateB = Object.keys(b)[0];
+
+      const dateObjA = new Date(dateA);
+      const dateObjB = new Date(dateB);
+
+      return dateObjA - dateObjB;
+    });
+
+    res.status(200).json(groupedEventsArray);
+  } catch (error) {
+    next(error);
+  }
 };
 
+const getEventsPromoters = async (req, res) => {
+  try {
+    // Obtener la fecha actual en la zona horaria de Argentina (GMT-3)
+    const todayInArgentina = DateTime.local()
+      .setZone("America/Argentina/Buenos_Aires")
+      .toISODate();
+
+    const result = await pool.query(`
+        SELECT e.*, p.* 
+        FROM event e
+        LEFT JOIN promoters p ON p.id = ANY(CAST(e.promoter_id AS uuid[]))
+        WHERE DATE(e.event_date) = '${todayInArgentina}'
+        ORDER BY
+          CASE
+            WHEN p.priority IS NOT NULL THEN p.priority -- Ordenar por prioridad definida
+            ELSE 9999 -- Poner los valores NULL al final
+          END
+      `);
+
+    if (result && result.rows && Array.isArray(result.rows)) {
+      const rows = result.rows;
+      res.status(202).json({ eventos: rows });
+    } else {
+      throw new Error("La consulta no devolvió un conjunto de filas.");
+    }
+  } catch (error) {
+    res.status(404).send("Error interno del servidor: " + error.message);
+  }
+};
 
 const createEvent = async (req, res) => {
-    try {
-        const {event_title, event_type, event_date, event_location, ticket_link, event_image, event_djs, event_city, event_promoter } = req.body.event;
-      
-        const formattedEventDate = new Date(event_date);
-        let promoter;
-        
-        if(event_promoter.length > 0){
-            promoter = event_promoter.map(promoter => promoter.id)
-        } else {
-            promoter = null
-        }
+  try {
+    const {
+      event_title,
+      event_type,
+      event_date,
+      event_location,
+      ticket_link,
+      event_image,
+      event_djs,
+      event_city,
+      event_promoter,
+    } = req.body.event;
 
-        const formattedType = event_type.join(' | ');
+    const formattedEventDate = new Date(event_date);
+    let promoter;
 
-        const querydate = ('SELECT ticket_link FROM event WHERE ticket_link = $1');
-        const valuesLink = [ticket_link];
+    if (event_promoter.length > 0) {
+      promoter = event_promoter.map((promoter) => promoter.id);
+    } else {
+      promoter = null;
+    }
 
-        let duplicateEvent = await pool.query(querydate, valuesLink);
-        duplicateEvent = duplicateEvent.rows
+    const formattedType = event_type.join(" | ");
 
-        if(duplicateEvent.length > 0 && !duplicateEvent[0]?.toLowerCase().includes('instagram') && !duplicateEvent[0]?.toLowerCase().includes('espacioro')){
-            res.status(404).send({ message: 'Ya existe este evento'});
-            return
-        }
+    const querydate = "SELECT ticket_link FROM event WHERE ticket_link = $1";
+    const valuesLink = [ticket_link];
 
- 
-        const query = `
+    let duplicateEvent = await pool.query(querydate, valuesLink);
+    duplicateEvent = duplicateEvent.rows;
+
+    if (
+      duplicateEvent.length > 0 &&
+      !duplicateEvent[0]?.toLowerCase().includes("instagram") &&
+      !duplicateEvent[0]?.toLowerCase().includes("espacioro")
+    ) {
+      res.status(404).send({ message: "Ya existe este evento" });
+      return;
+    }
+
+    const query = `
             INSERT INTO event(id, event_title, event_type, event_date, event_location, ticket_link, event_image, event_djs, city_id, promoter_id)
             VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
             RETURNING id;
         `;
 
-        const values = [uuidv4(), event_title, formattedType, formattedEventDate, event_location, ticket_link, event_image, event_djs, event_city.id, promoter];
+    const values = [
+      uuidv4(),
+      event_title,
+      formattedType,
+      formattedEventDate,
+      event_location,
+      ticket_link,
+      event_image,
+      event_djs,
+      event_city.id,
+      promoter,
+    ];
 
-        const result = await pool.query(query, values);
+    const result = await pool.query(query, values);
 
-        const insertedId = result.rows[0].id;
+    const insertedId = result.rows[0].id;
 
-        res.status(201).json({ message: 'Event created successfully', eventId: insertedId });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error creating event' });
-    }
+    res
+      .status(201)
+      .json({ message: "Event created successfully", eventId: insertedId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating event" });
+  }
 };
 
 const updateEvent = async (req, res) => {
-    try {
-        const eventId = req.params.id; // Assuming 'id' is the parameter name
+  try {
+    const eventId = req.params.id; // Assuming 'id' is the parameter name
 
-        const { title, date, image, link, djs } = req.body;
+    const { title, date, image, link, djs } = req.body;
 
-        const query = `
+    const query = `
             UPDATE event
             SET event_title = $1, event_date = $2, ticket_link = $3, event_image = $4, event_djs = $5
             WHERE id = $6;
         `;
 
-        const values = [title, date, link, image, djs, eventId];
+    const values = [title, date, link, image, djs, eventId];
 
-        await pool.query(query, values);
+    await pool.query(query, values);
 
-        res.status(200).json({ message: 'Event updated successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error updating event' });
-    }
+    res.status(200).json({ message: "Event updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating event" });
+  }
 };
 
 const deleteEvent = async (req, res) => {
-    try {
-        const eventIds = req.body.ids; // You need to set 'ids' as the key when passing the data
+  try {
+    const eventIds = req.body.ids; // You need to set 'ids' as the key when passing the data
 
-        const query = `
+    const query = `
             DELETE FROM event
             WHERE id = ANY($1);
         `;
 
-        const values = [eventIds];
+    const values = [eventIds];
 
-        await pool.query(query, values);
+    await pool.query(query, values);
 
-        res.status(200).json({ message: 'Events deleted successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error deleting events' });
-    }
+    res.status(200).json({ message: "Events deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting events" });
+  }
 };
 
 const searchEvent = async (req, res) => {
-    try {
-        const searchQuery = req.query.searchQuery;
+  try {
+    const searchQuery = req.query.searchQuery;
 
-        const query = `
+    const query = `
         SELECT * FROM event
         WHERE event_title ILIKE $1
            OR event_location ILIKE $1
@@ -154,109 +210,112 @@ const searchEvent = async (req, res) => {
            OR $1 = ANY(event_djs);
         `;
 
-        const values = [`%${searchQuery}%`]; // Using ILIKE for case-insensitive search
+    const values = [`%${searchQuery}%`]; // Using ILIKE for case-insensitive search
 
-        const result = await pool.query(query, values);
+    const result = await pool.query(query, values);
 
-        const events = result.rows;
+    const events = result.rows;
 
-        // Create an object to hold the grouped events
-        const groupedEvents = {};
-        
-        // Iterate through each event and group them by event_date
-        events.forEach(event => {
-            const eventDate = event.event_date;
-            if (!groupedEvents[eventDate]) {
-                groupedEvents[eventDate] = [];
-            }
-            groupedEvents[eventDate].push(event);
-        });
+    // Create an object to hold the grouped events
+    const groupedEvents = {};
 
-        // Convert the groupedEvents object into an array of objects
-        const groupedEventsArray = Object.keys(groupedEvents).map(date => ({ [date]: groupedEvents[date] }));
+    // Iterate through each event and group them by event_date
+    events.forEach((event) => {
+      const eventDate = event.event_date;
+      if (!groupedEvents[eventDate]) {
+        groupedEvents[eventDate] = [];
+      }
+      groupedEvents[eventDate].push(event);
+    });
 
-        res.status(200).json(groupedEventsArray);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error searching for events' });
-    }
+    // Convert the groupedEvents object into an array of objects
+    const groupedEventsArray = Object.keys(groupedEvents).map((date) => ({
+      [date]: groupedEvents[date],
+    }));
+
+    res.status(200).json(groupedEventsArray);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error searching for events" });
+  }
 };
-
 
 const filterEvents = async (req, res) => {
-    try {
-        const { date, city, type } = req.query;
+  try {
+    const { date, city, type } = req.query;
 
-        let query = "SELECT * FROM event WHERE TRUE";
-        const values = [];
+    let query = "SELECT * FROM event WHERE TRUE";
+    const values = [];
 
-        let paramCount = 1; // Initialize parameter counter
+    let paramCount = 1; // Initialize parameter counter
 
-        if (date) {
-            query += ` AND event_date = $${paramCount}`;
-            values.push(date);
-            paramCount++;
-        } else {
-            query += " AND (event_date IS NULL OR event_date = event_date)";
-        }
-
-        if (city) {
-            query += ` AND city_id = $${paramCount}`;
-            values.push(city);
-            paramCount++;
-        } else {
-            query += " AND (city_id IS NULL OR city_id = city_id)";
-        }
-
-        if (type) {
-            query += ` AND event_type = $${paramCount}`;
-            values.push(type);
-        } else {
-            query += " AND (event_type IS NULL OR event_type = event_type)";
-        }
-
-        const result = await pool.query(query, values);
-        const events = result.rows;
-
-        // Create an object to hold the grouped events
-        const groupedEvents = {};
-
-        // Iterate through each event and group them by event_date
-        events.forEach(event => {
-            const eventDate = event.event_date;
-            if (!groupedEvents[eventDate]) {
-                groupedEvents[eventDate] = [];
-            }
-            groupedEvents[eventDate].push(event);
-        });
-
-        // Convert the groupedEvents object into an array of objects
-        const groupedEventsArray = Object.keys(groupedEvents).map(date => ({ [date]: groupedEvents[date] }));
-
-        res.status(200).json(groupedEventsArray);
-    } catch (error) {
-        console.error("Error fetching events:", error);
-        res.status(500).json({ error: "An error occurred while fetching events" });
+    if (date) {
+      query += ` AND event_date = $${paramCount}`;
+      values.push(date);
+      paramCount++;
+    } else {
+      query += " AND (event_date IS NULL OR event_date = event_date)";
     }
+
+    if (city) {
+      query += ` AND city_id = $${paramCount}`;
+      values.push(city);
+      paramCount++;
+    } else {
+      query += " AND (city_id IS NULL OR city_id = city_id)";
+    }
+
+    if (type) {
+      query += ` AND event_type = $${paramCount}`;
+      values.push(type);
+    } else {
+      query += " AND (event_type IS NULL OR event_type = event_type)";
+    }
+
+    const result = await pool.query(query, values);
+    const events = result.rows;
+
+    // Create an object to hold the grouped events
+    const groupedEvents = {};
+
+    // Iterate through each event and group them by event_date
+    events.forEach((event) => {
+      const eventDate = event.event_date;
+      if (!groupedEvents[eventDate]) {
+        groupedEvents[eventDate] = [];
+      }
+      groupedEvents[eventDate].push(event);
+    });
+
+    // Convert the groupedEvents object into an array of objects
+    const groupedEventsArray = Object.keys(groupedEvents).map((date) => ({
+      [date]: groupedEvents[date],
+    }));
+
+    res.status(200).json(groupedEventsArray);
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    res.status(500).json({ error: "An error occurred while fetching events" });
+  }
 };
 
-
 const scrapLink = async (req, res) => {
-    try {
-        const LINK = req.body.link
-        let data = await linkScrap(LINK)
-        res.status(200).json(data)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const LINK = req.body.link;
+    let data = await linkScrap(LINK);
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
-    getEvents,
-    createEvent,
-    updateEvent,
-    deleteEvent,
-    searchEvent,
-    filterEvents,
-    scrapLink
-}
+  getEvents,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+  searchEvent,
+  filterEvents,
+  scrapLink,
+  getEventsPromoters,
+};
--- server/routes/events.routes.js
+++ server/routes/events.routes.js
@@ -1,35 +1,46 @@
-const {Router} = require('express')
-const session = require('express-session');
-const passport = require('passport');
-const {getEvents, createEvent, updateEvent, deleteEvent, searchEvent, filterEvents, scrapLink, UploadImage} = require('../controllers/event.controller')
-const {getCities} = require('../controllers/city.controller')
+const { Router } = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const {
+  getEvents,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+  searchEvent,
+  filterEvents,
+  scrapLink,
+  UploadImage,
+  getEventsPromoters,
+} = require("../controllers/event.controller");
+const { getCities } = require("../controllers/city.controller");
 // const {createUser, verifyUser} = require('../controllers/user.controller')
-// const {checkAdmin} = require('../middlewares/authMiddleware'); 
-const {getTypes} = require ('../controllers/type.controller');
-const { getDjs } = require('../controllers/djs.controller');
-const { getPromoters, postPromoters } = require('../controllers/promoters.controller');
+// const {checkAdmin} = require('../middlewares/authMiddleware');
+const { getTypes } = require("../controllers/type.controller");
+const { getDjs } = require("../controllers/djs.controller");
+const {
+  getPromoters,
+  postPromoters,
+} = require("../controllers/promoters.controller");
 
-
-const router = Router()
-
-
+const router = Router();
 
 //events
 
-router.get('/events', getEvents)
+router.get("/events", getEvents);
 
-router.get('/events/search', searchEvent)
+router.get("/events/search", searchEvent);
 
-router.get('/events/filters', filterEvents)
+router.get("/events/filters", filterEvents);
 
-router.post('/events', createEvent)
+router.post("/events", createEvent);
 
-router.put('/events/:id', updateEvent)
+router.put("/events/:id", updateEvent);
 
-router.delete('/events', deleteEvent)
+router.delete("/events", deleteEvent);
 
-router.post('/get-event-data', scrapLink)
+router.post("/get-event-data", scrapLink);
 
+router.get("/events-promoters", getEventsPromoters);
 
 //users
 
@@ -39,38 +50,28 @@ router.post('/get-event-data', scrapLink)
 //     res.render("login")
 // })
 
-
 // router.post('/login', passport.authenticate('local', {
 //     successRedirect: '/events',
 //     failureRedirect: '/login',
 //     failureMessage: true
 // }));
 
-
 //cities
 
-router.get('/cities', getCities)
-
+router.get("/cities", getCities);
 
 //tpyes
 
-router.get('/types', getTypes)
+router.get("/types", getTypes);
 
 //djs
 
-router.get('/djs', getDjs)
+router.get("/djs", getDjs);
 
 //promoters
 
-router.get('/promoters', getPromoters)
+router.get("/promoters", getPromoters);
 
-router.post('/create-promoters', postPromoters)
+router.post("/create-promoters", postPromoters);
 
-
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
