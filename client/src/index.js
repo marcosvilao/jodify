13f28091a5697a849edb6c098ef2ff85d16acf2e
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2024-01-31 09:20:14 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -10,17 +10,14 @@ import { LicenseInfo } from "@mui/x-license-pro";
 import { Analytics } from "@vercel/analytics/react";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-LicenseInfo.setLicenseKey(
-  "0bead84d3f16ad30d71410a8ae62882aTz03NzkzMixFPTE3MzA1NzY3MTEwMDAsUz1wcm8sTE09c3Vic2NyaXB0aW9uLEtWPTI="
-);
+const mui = process.env.REACT_APP_MUI;
+
+LicenseInfo.setLicenseKey(mui);
 
 Modal.setAppElement("#root");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const auth0ClientID = process.env.REACT_APP_AUTH0_CLIENTID;
-
-console.log(auth0Domain)
-console.log(auth0ClientID)
 
 root.render(
   <Provider store={store}>
