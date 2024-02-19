2d239cca20c5d272a1c2e1f1c6947687a83cb998
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2024-02-19 19:44:12 -0300

    animaciones fotter

--- client/src/components2/footer/footer.jsx
+++ client/src/components2/footer/footer.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./footer.module.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 
 function Footer() {
+  const [hideFooter, setHideFooter] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setHideFooter(true);
+    } else {
+      setHideFooter(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const onClickFacebook = () => {
     window.open("https://web.facebook.com/jodifyapp", "_blank");
   };
@@ -20,13 +35,29 @@ function Footer() {
   };
 
   return (
-    <div className={styles.body}>
+    <div
+      className={styles.body}
+      style={{
+        transform: `translateY(${hideFooter ? "100%" : "0"})`, // Desplaza el footer hacia abajo cuando se oculta
+      }}
+    >
       <div className={styles.container}>
         <div className={styles.containerLeft}>
           <p>©</p>
           <p>Jodify</p>
-          <FacebookIcon className={styles.icons} onClick={onClickFacebook} />
-          <InstagramIcon className={styles.icons} onClick={onClickInstagram} />
+          <FacebookIcon
+            onClick={onClickFacebook}
+            style={{
+              width: "15px",
+              height: "15px",
+              cursor: "pointer",
+              marginRight: "2px",
+            }}
+          />
+          <InstagramIcon
+            onClick={onClickInstagram}
+            style={{ width: "15px", height: "15px", cursor: "pointer" }}
+          />
         </div>
 
         <p className={styles.terminos} onClick={onClickTerminosYCondiciones}>
