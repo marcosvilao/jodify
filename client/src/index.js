2ff454458085ceb44837a961fd15759c3ce4325f
marcos <marcosvila96@gmail.com>
2024-03-27 02:05:09 -0300

    add service worker

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import Modal from "react-modal";
 import { LicenseInfo } from "@mui/x-license-pro";
 import { Analytics } from "@vercel/analytics/react";
 import { Auth0Provider } from "@auth0/auth0-react";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 
 const mui = process.env.REACT_APP_MUI;
 
@@ -37,3 +38,7 @@ root.render(
     </React.StrictMode>
   </Provider>
 );
+
+
+
+serviceWorkerRegistration.register();
