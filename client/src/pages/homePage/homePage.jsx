85cd6385225ef4054b8a9a9a96a70c6245dc1f54
marcos <marcosvila96@gmail.com>
2024-03-27 01:07:48 -0300

    time out set to 50

--- client/src/pages/homePage/homePage.jsx
+++ client/src/pages/homePage/homePage.jsx
@@ -194,7 +194,9 @@ function HomePage() {
             console.log(error);
           });
           if (event && event.ticket_link) {
+            setTimeout(() => {
               window.open(event.ticket_link, "_blank");
+            }, 50); 
           }
       };
 
