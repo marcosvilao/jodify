b8f4a4718e749776070dd63d799875d2b7f5d073
MaxMdr2022 <maxi_meder94@hotmail.com>
2024-04-30 16:52:54 -0300

    Added route user

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const event = require('../controllers/event.controller2.js')
 const promoter = require('../controllers/promoters.controller2.js')
 const generic = require('../controllers/generic.controller.js')
+const user = require('../controllers/user.controller2.js')
 
 const router = Router()
 
@@ -10,5 +11,7 @@ router.use('/events', event)
 router.use('/promoters', promoter)
 
 router.use('/jodify', generic)
+
+router.use('/user', user)
 
 module.exports = router
