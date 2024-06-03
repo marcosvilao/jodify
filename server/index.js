e872b1f066a00d4a8369dccf4fdcce13b025fb20
marcos <marcosvila96@gmail.com>
2024-06-03 20:53:37 -0300

    update

--- server/index.js
+++ server/index.js
@@ -1,54 +1,70 @@
-const express = require('express')
+const express = require('express');
 //-------------------
-const routes = require('./routes/routes.js')
+const routes = require('./routes/routes.js');
 //------------------
 // const passport = require('passport');
-const morgan = require('morgan')
-const cors = require('cors')
-// const session = require('express-session')
-// const FileStore = require('session-file-store')(session)
-// const {v4: uuidv4} = require('uuid')
+const morgan = require('morgan');
+const cors = require('cors');
+// const session = require('express-session');
+// const FileStore = require('session-file-store')(session);
+// const { v4: uuidv4 } = require('uuid');
 // const passportConfig = require('./passport-config');
-const flash = require('express-flash')
-const path = require('path')
-const bodyParser = require('body-parser')
+const flash = require('express-flash');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const eventRoutes = require('./routes/events.routes')
+// const eventRoutes = require('./routes/events.routes');
 
-const app = express()
+const app = express();
 
-app.use(function (req, res, next) {
-  var allowedOrigins = [
-    'https://jodify.com.ar',
-    'https://jodify.vercel.app',
-    'https://jodify-qa-client.vercel.app',
-    'https://l.instagram.com/',
-    'https://jodifynext.vercel.app',
-    'https://jodify-qa-next.vercel.app',
-  ]
-  var origin = req.headers.origin
+// Configuración de CORS
+const allowedOrigins = [
+  'https://jodify.com.ar',
+  'https://jodify.vercel.app',
+  'https://jodify-qa-client.vercel.app',
+  'https://l.instagram.com/',
+  'https://jodifynext.vercel.app',
+  'https://jodify-qa-next.vercel.app',
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+  optionsSuccessStatus: 204
+}));
+
+// Configurar otras cabeceras CORS
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin)
+    res.setHeader('Access-Control-Allow-Origin', origin);
   }
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
+});
 
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+// Otros middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('view engine', 'ejs') // Set EJS as the view engine
-app.set('views', path.join(__dirname, 'views')) // Set the views directory
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(cors());
-app.use(morgan('dev'))
-app.use(express.json())
+app.set('view engine', 'ejs'); // Set EJS as the view engine
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
 
 // app.use(flash());
 // app.use(
 //     session({
-//         genid:(req) => {
+//         genid: (req) => {
 //             return uuidv4()
 //         },
 //         store: new FileStore(),
@@ -56,7 +72,7 @@ app.use(express.json())
 //         resave: false,
 //         saveUninitialized: false
 //     })
-// )
+// );
 
 // app.use(passport.initialize());
 // app.use(passport.session());
@@ -67,13 +83,12 @@ app.use(express.json())
 
 // app.use(eventRoutes);
 
-
 app.use(routes);
 
 app.use((err, req, res, next) => {
   return res.json({
     message: err.message,
-  })
-})
+  });
+});
 
-app.listen(3001, () => console.log(`listening on port 3001`))
+app.listen(3001, () => console.log(`listening on port 3001`));
