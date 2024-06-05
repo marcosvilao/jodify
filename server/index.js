188897e7dee9e02279842b5a040a282e3e8e07fb
marcos <marcosvila96@gmail.com>
2024-06-05 20:29:57 -0300

    update cors

--- server/index.js
+++ server/index.js
@@ -1,73 +1,78 @@
-const express = require('express');
-const routes = require('./routes/routes.js');
-const morgan = require('morgan');
-const cors = require('cors');
-const flash = require('express-flash');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require('express')
+//-------------------
+const routes = require('./routes/routes.js')
+//------------------
+// const passport = require('passport');
+const morgan = require('morgan')
+const cors = require('cors')
+// const session = require('express-session')
+// const FileStore = require('session-file-store')(session)
+// const {v4: uuidv4} = require('uuid')
+// const passportConfig = require('./passport-config');
+const flash = require('express-flash')
+const path = require('path')
+const bodyParser = require('body-parser')
 
-require('dotenv').config();
+require('dotenv').config()
 
-const app = express();
+const eventRoutes = require('./routes/events.routes')
 
-// Configuración de CORS
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://jodify.com.ar',
-  'https://jodify.vercel.app',
-  'https://jodify-qa-client.vercel.app',
-  'https://l.instagram.com/',
-  'https://jodifynext.vercel.app',
-  'https://jodify-qa-next.vercel.app',
-];
+const app = express()
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    console.log(origin)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: '*',
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
-
-// Configurar otras cabeceras CORS
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
+app.use(function (req, res, next) {
+  var allowedOrigins = [
+    'https://jodify.com.ar',
+    'https://jodify.vercel.app',
+    'https://jodify-qa-client.vercel.app',
+    'https://l.instagram.com/',
+    'https://jodifynext.vercel.app',
+  ]
+  var origin = req.headers.origin
   if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Origin', origin)
   }
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(204); // Responder con 204 para solicitudes preflight (OPTIONS)
-  } else {
-    next();
-  }
-});
 
-// Otros middlewares
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
-app.set('view engine', 'ejs'); // Set EJS as the view engine
-app.set('views', path.join(__dirname, 'views')); // Set the views directory
+app.set('view engine', 'ejs') // Set EJS as the view engine
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cors());
+app.use(morgan('dev'))
+app.use(express.json())
+
+// app.use(flash());
+// app.use(
+//     session({
+//         genid:(req) => {
+//             return uuidv4()
+//         },
+//         store: new FileStore(),
+//         secret: process.env.PRIVATE_SESSION_KEY,
+//         resave: false,
+//         saveUninitialized: false
+//     })
+// )
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// passportConfig(passport);
+
+//-------------------------
+
+// app.use(eventRoutes);
+
 
 app.use(routes);
 
 app.use((err, req, res, next) => {
   return res.json({
     message: err.message,
-  });
-});
+  })
+})
 
-app.listen(3001, () => console.log(`listening on port 3001`));
+app.listen(3001, () => console.log('listening on port 3001'))
