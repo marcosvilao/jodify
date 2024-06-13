d0fe15f3f60a88ef742958ae8b3b5b8cc4065d01
MaxMdr2022 <maxi_meder94@hotmail.com>
2024-06-13 16:40:30 -0300

    Changes btn stiles

--- server/utils/nodeMailer/functions.js
+++ server/utils/nodeMailer/functions.js
@@ -12,6 +12,9 @@ const transporter = nodemailer.createTransport({
   },
 })
 
+const imageBtnDegrade =
+  'https://res.cloudinary.com/dzu7tm74o/image/upload/v1718300473/EVENTS/8800ff_c18fff_1920_1080_jqklr5.png'
+
 const mailOptionGeneratePassword = (email, username, token) => {
   return {
     from: 'Jodify',
@@ -25,8 +28,8 @@ const mailOptionGeneratePassword = (email, username, token) => {
         <div style="max-width: 600px; margin: 0 auto; padding: 20px;">
             <p>Hola ${username},</p>
             <p>Haz click en el link de abajo para confirmar el cambio de contraseña:</p>
-            <div style="padding: 20px;text-align: center;">
-                <a href="${process.env.HREF_ROOT}/reset-password/${token}" style="background-color: #C18FFF; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 100px;">
+            <div style="padding: 20px; text-align: center;">
+                <a href="${process.env.HREF_ROOT}/reset-password/${token}" style="background-image: url(${imageBtnDegrade}); color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 100px; background-size: cover;">
                     Ir a Jodify
                 </a>
             </div>
@@ -62,8 +65,8 @@ const mailOptionValidateEmail = (email, username) => {
         <p>Hola ${username}</p>
         <p>¿Cómo estas?</p>
         <p>Haz click en el link de abajo para confirmar tu correo electrónico.</p>
-        <div style="padding: 20px;text-align: center;">
-            <a href="${process.env.HREF_ROOT}/" style="background-color: #C18FFF; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 100px;">
+        <div style="padding: 20px; text-align: center;">
+            <a href="${process.env.HREF_ROOT}/" style="background-image: url(${imageBtnDegrade}); color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 100px; background-size: cover;">
                 Ir a Jodify
             </a>
         </div>
@@ -99,8 +102,8 @@ const mailOptionWelcomeForm = (email, username, adminName, token) => {
           <p style="margin: 0 0 10px;">Hola ${username},</p>
           <p style="margin: 0 0 20px;">Soy ${adminName}, un gusto hablar con vos y conocer más acerca de tu productora. Te creamos un <a href="${process.env.HREF_ROOT}/register-promoters/${token}" style="color: #C18FFF; text-decoration: underline; cursor: pointer;">link</a> temporal para que puedas registrarte con el correo electrónico ${email} y así empezar a compartir tus eventos.</p>
           <p style="margin: 0 0 20px;"><strong>¡Quiero darte la bienvenida a Jodify!</strong></p>
-          <div style="text-align: center; margin: 20px 0;">
-              <a href="${process.env.HREF_ROOT}/register-promoters/${token}" style="background-color: #C18FFF; color: white; padding: 15px 32px; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 100px; cursor: pointer;">
+          <div style="padding: 20px; text-align: center;">
+              <a href="${process.env.HREF_ROOT}/register-promoters/${token}" style="background-image: url(${imageBtnDegrade}); color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 100px; background-size: cover;">
                   Registrarse
               </a>
           </div>
@@ -113,6 +116,42 @@ const mailOptionWelcomeForm = (email, username, adminName, token) => {
       </div>
   </body>
 
+      `,
+  }
+}
+
+const mailOptionUserPromoterRegister = (email, username) => {
+  return {
+    from: 'Jodify',
+    to: `${email}`,
+    subject: 'Nueva cuenta Jodify',
+    html: `
+    <body style="font-family: Arial, sans-serif; margin:0; padding:0;">
+    <div style="max-width: 600px; margin: 0 auto; background-color: #0C0C0C; text-align: center; padding: 20px;">
+            <img src=${logo} alt="Logo" style="width: 150px; height: auto; display: inline-block;">
+        </div>
+        <div style="max-width: 600px; margin: 0 auto; padding: 20px;">
+            <p>Hola ${username},</p>
+            <div style="margin-bottom:5;">Tu cuenta fue creada con éxito,</div>
+            <div style="color:#8800FF;"><strong>¡Queremos darte la bienvenida a Jodify!</strong></div>
+            <div style="padding: 20px; text-align: center;">
+            <a href="${process.env.HREF_ROOT}/login-promoters" style="background-image: url(${imageBtnDegrade}); color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 100px; background-size: cover;">
+                Iniciar Sesión
+            </a>
+            </div>
+
+              <div style="margin-bottom:5;">Estamos aquí para ayudarte en cada paso del camino.</div>
+              <div style="margin-bottom:50;">¡Esperamos que disfrutes tu experiencia!</div>
+          
+
+            <div style="margin-bottom: 0;">Abrazo.</div>
+            <div style="margin-bottom: 0;">Equipo de Jodify.</div>
+
+        </div>
+        <div style="max-width: 600px; margin: 0 auto; background-color: #0C0C0C; color: #FFFFFF; padding: 10px; text-align: center;">
+            Jodify © 2024
+        </div>
+    </body>
       `,
   }
 }
@@ -136,4 +175,5 @@ module.exports = {
   mailOptionGeneratePassword,
   mailOptionValidateEmail,
   mailOptionWelcomeForm,
+  mailOptionUserPromoterRegister,
 }
