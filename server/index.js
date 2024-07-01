32a814b53c578e00b1f7802615712fa9d1f5db42
marcos <marcosvila96@gmail.com>
2024-07-01 13:24:31 -0300

    update ips

--- server/index.js
+++ server/index.js
@@ -1,90 +1,72 @@
-const express = require('express')
-//-------------------
-const routes = require('./routes/routes.js')
-//------------------
-// const passport = require('passport');
-const morgan = require('morgan')
-const cors = require('cors')
-// const session = require('express-session')
-// const FileStore = require('session-file-store')(session)
-// const {v4: uuidv4} = require('uuid')
-// const passportConfig = require('./passport-config');
-const flash = require('express-flash')
-const path = require('path')
-const bodyParser = require('body-parser')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const flash = require('express-flash');
+const path = require('path');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-require('dotenv').config()
+const routes = require('./routes/routes.js');
+const eventRoutes = require('./routes/events.routes');
 
-const eventRoutes = require('./routes/events.routes')
+const app = express();
 
-const app = express()
-
+// Lista de IPs permitidas
 const allowedIPs = [
-  '192.168.1.8',
+  '123.456.789.000', // Replace with actual allowed IPs
+  '987.654.321.000',
+  // Add more IPs as needed
 ];
 
-const ipFilter = (req, res, next) => {
+// Lista de dominios permitidos
+const allowedOrigins = [
+  'https://jodify.com.ar',
+  'https://jodify-qa-client.vercel.app',
+  'https://l.instagram.com/',
+  'https://jodifynext.vercel.app',
+];
+
+// Middleware para verificar el origen y las IPs
+const originAndIPFilter = (req, res, next) => {
+  const origin = req.headers.origin;
   const clientIP = req.ip || req.connection.remoteAddress;
-  if (allowedIPs.includes(clientIP)) {
-    next();
-  } else {
-    res.status(403).json({ message: 'Forbidden' });
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    return next();
   }
+
+  if (allowedIPs.includes(clientIP)) {
+    return next();
+  }
+
+  res.status(403).json({ message: 'Forbidden' });
 };
 
-app.use(function (req, res, next) {
-  var allowedOrigins = [
-    'https://jodify.com.ar',
-    'https://jodify-qa-client.vercel.app',
-    'https://l.instagram.com/',
-    'https://jodifynext.vercel.app',
-  ]
-  var origin = req.headers.origin
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin)
-  }
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+app.set('view engine', 'ejs'); // Set EJS as the view engine
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('view engine', 'ejs') // Set EJS as the view engine
-app.set('views', path.join(__dirname, 'views')) // Set the views directory
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(ipFilter)
 app.use(cors());
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(morgan('dev'));
+app.use(express.json());
 
-// app.use(flash());
-// app.use(
-//     session({
-//         genid:(req) => {
-//             return uuidv4()
-//         },
-//         store: new FileStore(),
-//         secret: process.env.PRIVATE_SESSION_KEY,
-//         resave: false,
-//         saveUninitialized: false
-//     })
-// )
+// Aplicar el middleware a todas las rutas
+app.use(originAndIPFilter);
 
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passportConfig(passport);
-
-//-------------------------
-
-// app.use(eventRoutes);
-
-
+// Rutas de la aplicación
 app.use(routes);
+// app.use(eventRoutes);
 
 app.use((err, req, res, next) => {
   return res.json({
     message: err.message,
-  })
-})
+  });
+});
 
-app.listen(3001, () => console.log('listening on port 3001'))
+app.listen(3001, () => console.log('listening on port 3001'));
