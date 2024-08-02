f436918e312823548193cc649d61de493c2705c0
MaxMdr2022 <maxi_meder94@hotmail.com>
2024-08-02 13:06:30 -0300

    Fix validate uuidV5

--- server/utils/regex.js
+++ server/utils/regex.js
@@ -1,4 +1,4 @@
-const UUIDV4RegEx = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+const UUIDV4RegEx = /^[0-9a-f]{8}-[0-9a-f]{4}-[45][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
 
 module.exports = {
   UUIDV4RegEx,
